212432b884da8036cdf821a0a53ea84e07deafbb
aji.prasetyo <aji.prasetyo@bayarind.id>
2024-09-28 21:12:21 +0700

    finish relayouting activity

--- src/views/Activity/Activity.vue
+++ src/views/Activity/Activity.vue
@@ -1,34 +1,11 @@
 <script setup>
-import { useActivityStore } from '@/stores/activity.store.js'
-import { useHomeStore } from '@/stores/home.store'
-
-const stores = useActivityStore()
-const homeStores = useHomeStore()
-
-const router = useRouter()
-
-const goToDetail = (id) => {
-  // appStores.setCurrentActivityId(id)
-  router.push(`/activity/${id}`)
-}
-
-onMounted(() => {
-  stores.getActivityCards()
-})
+import acivity from '@/assets/images/activity-img.jpg'
 </script>
 
 <template>
-  <VContainer class="pt-1 justify-space-between d-flex flex-wrap">
-    <CardComponent
-      max-width="48%"
-      class="mb-4"
-      v-for="item in stores.filteredCards"
-      :key="item.id"
-      :title="item.title"
-      :location="item.location"
-      :amount="item.amount"
-      :img="item.image"
-      @click="goToDetail(item.id)"
-    />
-  </VContainer>
+  <VImg
+    :src="acivity"
+    cover
+    style="height: calc(100vh - 80px); width: 100%; object-fit: cover; object-position: center"
+  />
 </template>
